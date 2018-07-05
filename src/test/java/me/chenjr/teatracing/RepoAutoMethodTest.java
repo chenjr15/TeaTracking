9fb9e970102624ebe8dfabd3589a02ea69f1cc43
chenjr15 <icjr@qq.com>
2018-07-05 23:05:24 +0800

    添加测试代码

--- src/test/java/me/chenjr/teatracing/RepoAutoMethodTest.java
+++ src/test/java/me/chenjr/teatracing/RepoAutoMethodTest.java
@@ -3,7 +3,9 @@ package me.chenjr.teatracing;
 
 
 import me.chenjr.teatracing.domain.Factory;
+import me.chenjr.teatracing.domain.TeaPkg;
 import me.chenjr.teatracing.repositories.FactoryRepository;
+import me.chenjr.teatracing.service.TeaPkgService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,14 +19,33 @@ public class RepoAutoMethodTest {
 
     @Autowired
     FactoryRepository factoryRepository;
+    @Autowired
+    TeaPkgService teaPkgService;
 
     @Test
     public void  testCURD(){
-        String name = "制茶师阿福";
-        factoryRepository.save(new Factory(name));
-        Assert.assertNotNull( "name Not Found!",factoryRepository.findByName(name));
-        Assert.assertNull(factoryRepository.findByName("制茶师"));
+        String name = "fff";
 
+        Factory factory ;
+
+        factoryRepository.saveAndFlush(new Factory(name));
+
+        factory =factoryRepository.findByName(name);
+        Assert.assertNotNull( factory);
+
+        factoryRepository.deleteById(factory.getId());
+
+        Factory ggg = factoryRepository.findByName(factory.getName());
+        System.out.println(ggg);
+        Assert.assertNull(ggg);
+
+    }
+//    @Test
+    public void testFindById(){
+        long id = 1;
+
+        TeaPkg teaPkg = teaPkgService.findById(id);
+        Assert.assertNotNull(teaPkg);
     }
 
 }
